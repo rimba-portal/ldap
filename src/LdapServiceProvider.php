e6ce397766c47a49edad71a93a549143983b9f73
Faros <114036707+a-faros@users.noreply.github.com>
2026-08-13 11:56:30 +0800

    Release v1.0.4

--- src/LdapServiceProvider.php
+++ src/LdapServiceProvider.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Rimba\Ldap;
 
 use Rimba\Base\Services\BitesServiceProvider;
-use Rimba\Who\Services\LdapAuthService;
+use Rimba\Ldap\Services\LdapAuthService;
 
 class LdapServiceProvider extends BitesServiceProvider
 {
