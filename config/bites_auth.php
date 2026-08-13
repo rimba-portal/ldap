44e6019dd04f78b2523d57b93252feea77d56aec
Faros <114036707+a-faros@users.noreply.github.com>
2026-08-13 16:16:43 +0800

    Release v1.0.7

--- config/bites_auth.php
+++ config/bites_auth.php
@@ -1,12 +1,12 @@
 <?php
 
 declare(strict_types=1);
+
 use App\Models\User;
 use Rimba\People\Models\Staff;
 
 return [
     'connection' => env('RIMBA_LDAP_CONNECTION', 'default'),
-
     'attributes' => [
         'identifier' => 'samaccountname',
         'email' => 'mail',
@@ -15,17 +15,8 @@ return [
         'common_name' => 'cn',
         'staff_number' => 'employeenumber',
     ],
-
-    'local_user_model' => env(
-        'RIMBA_LDAP_LOCAL_USER_MODEL',
-        config('auth.providers.users.model', User::class),
-    ),
-
-    'staff_model' => env(
-        'RIMBA_LDAP_STAFF_MODEL',
-        Staff::class,
-    ),
-
+    'local_user_model' => env('RIMBA_LDAP_LOCAL_USER_MODEL', config('auth.providers.users.model', User::class)),
+    'staff_model' => env('RIMBA_LDAP_STAFF_MODEL', Staff::class),
     'staff_number_column' => 'staff_no',
     'staff_user_column' => 'user_id',
 ];
